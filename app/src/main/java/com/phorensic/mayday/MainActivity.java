68e00b6ce23f24efec4b3ac15ab225e2280c5be8
Matt Mead <matt.a.mead@gmail.com>
2019-03-01 02:46:12 -0800

    Unnecessary check for Build Version
    Since just building for my phone

--- app/src/main/java/com/phorensic/mayday/MainActivity.java
+++ app/src/main/java/com/phorensic/mayday/MainActivity.java
@@ -3,7 +3,6 @@ package com.phorensic.mayday;
 import android.app.Activity;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
 
@@ -15,14 +14,12 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         // create default notification channel
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            final String channelId = getString(R.string.default_floatingview_channel_id);
-            final String channelName = getString(R.string.default_floatingview_channel_name);
-            final NotificationChannel defaultChannel = new NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_MIN);
-            final NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-            if (manager != null) {
-                manager.createNotificationChannel(defaultChannel);
-            }
+        final String channelId = getString(R.string.default_floatingview_channel_id);
+        final String channelName = getString(R.string.default_floatingview_channel_name);
+        final NotificationChannel defaultChannel = new NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_MIN);
+        final NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        if (manager != null) {
+            manager.createNotificationChannel(defaultChannel);
         }
 
         if (savedInstanceState == null) {
